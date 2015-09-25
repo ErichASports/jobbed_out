89bbe6e6a7182003fc89c66f883f2a545b7407a4
Erich Abbott <abbotterich@gmail.com>
2015-09-25 10:44:35 -0500

    boilerplate setup

---
+++ db/migrate/20150925150922_sorcery_core.rb
@@ -0,0 +1,16 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,            :null => false
+      t.string :crypted_password
+      t.string :salt
+      #jobbed_out specific fields 
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
