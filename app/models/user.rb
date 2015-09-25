6693edb928a816faffe25cfd9c1ed3c7fadb0db8
mapineda Pineda <mapineda1222@gmail.com>
2015-09-25 17:11:07 -0500

    fixing errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :posts
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
